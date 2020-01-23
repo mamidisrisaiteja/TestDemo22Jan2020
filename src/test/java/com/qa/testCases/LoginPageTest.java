28e7e91a199c8f5cf5733eb194b323e368ed5d86
sai teja <mamidisrisaiteja@gmail.com>
2020-01-23 16:30:24 +0530

    tester1

--- src/test/java/com/qa/testCases/LoginPageTest.java
+++ src/test/java/com/qa/testCases/LoginPageTest.java
@@ -1,18 +1,15 @@
 package com.qa.testCases;
 
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.qa.base.TestBase;
 import com.qa.pages.HomePage;
 import com.qa.pages.LoginPage;
+//import com.qa.util.TestUtil;
 import com.qa.util.TestUtil;
+
 
 public class LoginPageTest extends TestBase {
 	
@@ -39,18 +36,20 @@ public class LoginPageTest extends TestBase {
 		System.out.println("hai");
 	}
 	
+	@DataProvider
+	public Object[][] getCRMTestData(){
+		Object data[][] = TestUtil.getTestData("contacts");
+				//TestUtil.getTestData("contacts");
+		return data;
+	}
+	
 	@Test(dataProvider="getCRMTestData")
 	public void loginTest(String urName, String pasword) {
 		homePage=loginPage.login(urName, pasword);
 		
 	}
-@DataProvider
-	public Object[][] getCRMTestData() {
-		
-		Object data [][]=TestUtil.getTestData("contacts");
-		return data;
-		
-	}	
+	
+	
 	
 //	@Test
 //	public void loginTest() {
@@ -58,9 +57,9 @@ public class LoginPageTest extends TestBase {
 //		homePage=loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
 //}
 	
-	@AfterMethod
-	public void tearDown() {
-		driver.quit();
-	}
+//	@AfterMethod
+//	public void tearDown() {
+//		driver.quit();
+//	}
 
 }
